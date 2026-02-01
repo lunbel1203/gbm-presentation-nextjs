cf803f64586bac0eb2b80587ba2bd7a12995c039
Jose Luis Beltre <josebeltre@poderjudicial.gob.do>
2026-02-01 16:48:25 -0400

    Se agrego livechat y el favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
+import LiveChat from "@/components/LiveChat";
 
 const montserrat = Montserrat({
   variable: "--font-montserrat",
@@ -27,6 +28,7 @@ export default function RootLayout({
         className={`${montserrat.variable} font-montserrat antialiased`}
       >
         {children}
+        <LiveChat />
       </body>
     </html>
   );
---
+++ src/components/LiveChat.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { LiveChatWidget } from "@livechat/widget-react";
+
+export default function LiveChat() {
+    return <LiveChatWidget license="15241866" />;
+}
